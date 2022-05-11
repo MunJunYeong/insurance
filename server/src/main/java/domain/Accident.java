50b1a6f408c1eaef7a761eb18b7917f9fabf0ffa
DESKTOP-G145FC7\moon <msw711666@naver.com>
2022-05-11 14:55:48 +0900

    start distributed programming 1

---
+++ server/src/main/java/domain/Accident.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class Accident {
+
+    private int accidentIdx;
+    private int accidentDate;
+    private String content;
+    private int damagePrice;
+    private int employeeIdx;
+    private int insuranceIdx;
+    private boolean lawsuit;
+    private int userIdx;
+
+    public Accident(){
+
+    }
+
+
+}
---
+++ server/src/main/java/domain/ex_insurance/Travel.java
@@ -0,0 +1,13 @@
+package domain.ex_insurance;
+
+import domain.Insurance;
+
+public class Travel extends Insurance {
+    private int travelIdx;
+    private int country;
+    private int period;
+
+    public Travel(){
+
+    }
+}
